76dde1e3646e1d8e040e6a05ff12a8e96a32039c
Dreeke <andre.hartwich@gmail.com>
2014-07-14 15:38:53 +0200

    added flask-blog directory

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -5,9 +5,19 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
+@app.route("/test/<search_query>")
+
+
+def search(search_query):
+	return search_query
+
+def search():
+    return "Hello"
 
 # define the view using a function, which returns a string
 def hello_world():
